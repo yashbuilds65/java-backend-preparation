c43b68184ace06695e99a5ed44837909d691abd1
yashbuilds65 <shewaleyash7058@gmail.com>
2026-09-07 20:16:53 +0530

    Day 3: Reverse array program
    Fix syntax errors and improve array initialization.

---
+++ Day03/reverseArray.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class reverseArray{
+    public static void main(String[] args) {
+        int n;
+        Scanner sc= new Scanner(System.in);
+        n=sc.nextInt;
+        int arr= new arr[n];
+        for(i=0;i<array.length();i++){
+            arr[i]=sc.nextInt();
+            
+        }
+    }
+}
